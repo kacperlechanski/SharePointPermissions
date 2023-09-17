b07a95e1f69fa559747f6a642c8afb574f5d8a69
kacperlechanski <kacperlechanski19@o2.pl>
2023-09-17 18:36:19 +0200

    Add login function

--- Permissions.ps1
+++ Permissions.ps1
@@ -1,1 +1,12 @@
 #Skrypt sprawdzający uprawnień do folderów w bibliotece dokumentów na SharePoint
+
+#Włącz skrypt od aktualizacji lub instalacji modułów
+#.\Modules.ps1
+
+#Połącz za pomocą modułu PnP.Powershell
+function ConnectSharePoint{
+    $url = Read-Host "Podaj link do witryny SharePoint: "
+    Connect-PnPOnline -Url $url -Interactive
+}
+
+ConnectSharePoint
