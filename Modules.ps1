e030c09eff6a689c46e5cbee6aba9391823c0b4a
kacperlechanski <kacperlechanski19@o2.pl>
2023-09-17 18:23:32 +0200

    Finish Modules.ps1 file

--- Modules.ps1
+++ Modules.ps1
@@ -1,10 +1,15 @@
-#Skrypt Powershell do aktualizacji wymaganych modułów w celu pobrania uprawnień z biblioteki SharePoint
+#Skrypt Powershell do aktualizacji wymaganych modułów w celu pobrania uprawnień z biblioteki SharePoint.
+#Stworzony przez Kacper Lechański
+#Wersja 1.0
+
+Write-Host "Skrypt wykonujacy instalacje lub aktualizacje modulow w PowerShell" -ForegroundColor Green
+Start-Sleep -Seconds 3
 
 #Ustaw repozytorium "PSGallery jakoe zaufane"
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 
 Write-Host "Sprawdzam czy wymagane moduly sa zainstalowane.." -ForegroundColor "Yellow"
-
+Start-Sleep -Seconds 3
 
 #Instalacja lub aktualizacja modułu MSOnline
 function InstallUpdateMSOnline{
@@ -19,25 +24,28 @@ function InstallUpdateMSOnline{
         Install-Module -Name MSOnline -Force
         Get-InstalledModule -Name MSOnline
     }
+    Start-Sleep -Seconds 3
 }
 
 
 #Instalacja lub aktualizacja modułu PnP.Powershell
-function InstallUpdateMSOnline{
-    Write-Host "Sprawdzam czy wymagane moduly sa zainstalowane.." -ForegroundColor "Yellow"
-    $msonline = Get-InstalledModule -Name MSOnline
-    if($msonline){
-        Write-Host "Modul 'MSOnline' jest juz zainstalowany. Wykonuje aktualizacje.." -ForegroundColor Yellow
-        Update-Module -Name MSOnline
-        Get-InstalledModule -Name MSOnline
+function InstallUpdatePnPPowershell{
+    $pnppowershell = Get-InstalledModule -Name PnP.Powershell
+    if($pnppowershell){
+        Write-Host "Modul 'PnP.Powershell' jest juz zainstalowany. Wykonuje aktualizacje.." -ForegroundColor Yellow
+        Update-Module -Name PnP.Powershell
+        Get-InstalledModule -Name PnP.Powershell
     }
     else{
-        Write-Host "Brak modulu 'MSOnline'. Rozpoczynam instalacje.." -ForegroundColor Red
-        Install-Module -Name MSOnline -Force
-        Get-InstalledModule -Name MSOnline
+        Write-Host "Brak modulu 'PnP.Powershell'. Rozpoczynam instalacje.." -ForegroundColor Red
+        Install-Module -Name PnP.Powershell -Force
+        Get-InstalledModule -Name PnP.Powershell
     }
+    Start-Sleep -Seconds 3
 }
 
-
+#Wywowałanie funkcji
 InstallUpdateMSOnline
 InstallUpdatePnPPowershell
+
+Read-Host "Wcisnij ENTER, aby zakonczyc.."
